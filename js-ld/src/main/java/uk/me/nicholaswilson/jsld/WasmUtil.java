e49a7b70d6395241165c71af26e2c493ffb1fc14
Nicholas Wilson <nicholas.wilson@realvnc.com>
2018-02-26 21:00:00 +0000

    js-ld: Fix decoding of global initialisers, adding LEB64 decoding too

--- js-ld/src/main/java/uk/me/nicholaswilson/jsld/WasmUtil.java
+++ js-ld/src/main/java/uk/me/nicholaswilson/jsld/WasmUtil.java
@@ -40,7 +40,7 @@ class WasmUtil {
   }
 
   public static int getUleb32(ByteBuffer buffer) {
-    long bits = getLeb32(buffer, false);
+    long bits = getLeb5(buffer, false);
     if ((bits & ~0xffff_ffffL) != 0) {
       // The last byte cannot have the top three bits set, which would take the
       // value out of a U32 range.
@@ -52,22 +52,23 @@ class WasmUtil {
   }
 
   public static int getSleb32(ByteBuffer buffer) {
-    long bits = getLeb32(buffer, true);
+    long bits = getLeb5(buffer, true);
     long sign = bits & ~0x7fff_ffffL;
     if (sign != 0 && sign != ~0x7fff_ffffL) {
-      // The top three bits must be set to the same value, or else the value will be out of the
-      // S32 range.
+      // The top three bits must be set to the same value, or else the value
+      // will be out of the S32 range.
       throw new LdException("Invalid Wasm file: bad SLEB32");
     }
     return (int)bits;
   }
 
-  private static long getLeb32(ByteBuffer buffer, boolean signExtend) {
+  /** Reads up to five bytes in LEB format */
+  private static long getLeb5(ByteBuffer buffer, boolean signExtend) {
     long bits = 0;
     int shift = 0;
     for (int i = 0; i < 5; ++i) {
       byte b = buffer.get();
-      bits |= (long)(b & 0x7f) << shift;
+      bits |= (b & 0x7fL) << shift;
       if ((b & 0x80) == 0) {
         if (signExtend && (b & 0x40) != 0)
           bits |= (~0) << (shift + 7);
@@ -77,6 +78,73 @@ class WasmUtil {
     }
     // A value in the range 0..2^32-1 cannot take more than five bytes
     throw new LdException("Invalid Wasm file: bad LEB32");
+  }
+
+  public static long getUleb64(ByteBuffer buffer) {
+    byte[] bits = getLeb10(buffer, false);
+    if (bits[8] != 0) {
+      // The last byte cannot have the top bits set, which would take the value
+      // out of a U64 range.
+      throw new LdException("Invalid Wasm file: bad ULEB64");
+    }
+    if ((Byte.toUnsignedInt(bits[7]) & 0x80) != 0)
+      throw new LdException("Unsupported Wasm file: ULEB64 >= 2^63");
+    return lebValue(bits);
+  }
+
+  public static long getSleb64(ByteBuffer buffer) {
+    byte[] bits = getLeb10(buffer, true);
+    int sign = (Byte.toUnsignedInt(bits[8]) << 1) |
+      (Byte.toUnsignedInt(bits[7]) >> 7);
+    if (sign != 0 && sign != 0x1ffL) {
+      // The top bits must be set to the same value, or else the value will be
+      // out of the S64 range.
+      throw new LdException("Invalid Wasm file: bad SLEB64");
+    }
+    return lebValue(bits);
+  }
+
+  /** Reads up to ten bytes in LEB format */
+  private static byte[] getLeb10(ByteBuffer buffer, boolean signExtend) {
+    byte[] bits = new byte[9]; // little-endian
+    int shift = 0;
+    for (int i = 0; i < 10; ++i) {
+      byte b = buffer.get();
+      lebShiftOr(bits, (byte)(b & 0x7f), shift); // bits |= (b & 0x7f) << shift
+      if ((b & 0x80) == 0) {
+        if (signExtend && (b & 0x40) != 0)
+          lebExtend(bits, shift + 7);
+        return bits;
+      }
+      shift += 7;
+    }
+    // A value in the range 0..2^64-1 cannot take more than ten bytes
+    throw new LdException("Invalid Wasm file: bad LEB64");
+  }
+
+  private static long lebValue(byte[] bits) {
+    long value = 0L;
+    for (int i = 0; i < 8; ++i)
+      value |= Byte.toUnsignedLong(bits[i]) << (8 * i);
+    return value;
+  }
+
+  private static void lebShiftOr(byte[] bits, byte b, int shift) {
+    int shiftLowByte = shift / 8, shiftBits = shift % 8;
+    if (shiftBits == 0) {
+      bits[shiftLowByte] |= b;
+    } else {
+      bits[shiftLowByte] |= b << shiftBits;
+      bits[shiftLowByte + 1] |= b >>> (8 - shiftBits);
+    }
+  }
+
+  private static void lebExtend(byte[] bits, int shift) {
+    int shiftLowByte = shift / 8, shiftBits = shift % 8;
+    bits[shiftLowByte] |= (byte)0xff << shiftBits;
+    for (shift += 8 - shiftBits; shift < 9 * 8; shift += 8) {
+      bits[shift / 8] = (byte)0xff;
+    }
   }
 
   public static String getString(ByteBuffer buffer) {
@@ -121,11 +189,11 @@ class WasmUtil {
       if (op == OPCODE_GET_GLOBAL) {
         getUleb32(buffer);
       } else if (op == OPCODE_CONST_I32) {
-        buffer.getInt();
+        getSleb32(buffer);
       } else if (op == OPCODE_CONST_F32) {
         buffer.getFloat();
       } else if (op == OPCODE_CONST_I64) {
-        buffer.getLong();
+        getSleb64(buffer);
       } else if (op == OPCODE_CONST_F64) {
         buffer.getDouble();
       } else {
